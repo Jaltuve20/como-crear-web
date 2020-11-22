54c222a333f9d8f94fe0a46f9b060165a9fd57da
Jaltuve20 <74807559+Jaltuve20@users.noreply.github.com>
2020-11-21 22:05:55 -0400

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+const hamburguer = document.querySelector('.icono-menu')
+const menu = document.querySelector('.menu')
+
+
+console.log(menu)
+console.log(hamburguer)
+
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread')
+        && e.target != menu && e.target != hamburguer    ){
+            menu.classList.toggle("spread")
+    }
+
+})
+
+
